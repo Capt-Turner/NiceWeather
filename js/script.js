534414b2504813f6576ac92a04e9244aa3d8b226
Seth Turner <sethhet@gmail.com>
2022-10-11 21:33:11 -0600

    basic weather fetch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-const searchForm = document.querySelector('#search-form');
-const searchInp = document.querySelector('#search-input');
+const APIkey='';
+const searchForm=document.querySelector('#search-form');
+const searchInp=document.querySelector('#search-input');
 
-function getCurrentWeath(){
+function displayItems(city,data){
+    displayCurrWeath();
+    displayForecast();
+};
+
+function fetchWeath(location){
+    var {lat}=location;
+    var {lon}=location;
+    var city=location.name;
+    var apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`
     
-}
+    fetch(apiUrl)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            displayItems(city,data);
+        })
+        .catch(function(err){
+            console.error(err);
+        });
+};
