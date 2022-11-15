76812633a05e53da7bc4d0ebbf2452652ab25e80
Seth Turner <sethhet@gmail.com>
2022-11-14 18:19:26 -0700

    added current weather function

--- js/script.js
+++ js/script.js
@@ -3,6 +3,45 @@ const weathAPIrootUrl='https:/api.openweathermap.org';
 const searchForm=document.querySelector('#search-form');
 const searchInp=document.querySelector('#search-input');
 
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+
+function displayCurrWeath(){
+    var date=dayjs().tz(timezone).format('M/D/YYYY');
+    var temp=weather.temp;
+    var windspd=weather.wind_speed;
+    var humid=weather.humidity;
+    var uvi=weather.uvi;
+
+    var card=document.createElement('div');
+    var cardBod=document.createElement('div');
+    var head=document.createElement('h3');
+    var tempEl=document.createElement('p');
+    var windspdEl=document.createElement('p');
+    var humidEl=document.createElement('p');
+    var uviEl=document.createElement('p');
+    
+
+    card.setAttribute('class','card');
+    cardBod.setAttribute('class','card-body');
+    head.setAttribute('class','card-title');
+    tempEl.setAttribute('class','card-text');
+    windspdEl.setAttribute('class','card-text');
+    humidEl.setAttribute('class','card-text');
+
+    head.textContent=`${city} (${date})`;
+    tempEl.textContent=`Temperature: ${temp}°F`;
+    windspdEl.textContent=`Wind: ${windspd} MPH`;
+    humidEl.textContent=`Humidity: ${humidity}%`;
+    uviEl.textContent=`UV index: ${uvi}`;
+    currWeatherContainter.innerHTML='';
+
+    card.append(cardBod);
+    cardBod.append(head,tempEl,windspdEl,humidEl,uviEl);
+    currWeatherContainter.append(card);
+}
+
 function displayItems(city,data){
     displayCurrWeath();
     displayForecast();
